3464760b3c857fd816d5f0ac5c509b16eda99ac4
zackattacknyu <zackattacknyu@gmail.com>
2014-05-12 15:00:59 -0700

    started eye reconstruction project

---
+++ zach/initScript.m
@@ -0,0 +1,23 @@
+%IMPORTANT: Make sure to put this file in separately
+fname = 'rabbit central reduced 2.tif';
+
+info = imfinfo(fname);
+num_images = numel(info);
+image1 = im2double(imread(fname,1,'Info',info));
+imageSize = size(image1);
+allImages = zeros(imageSize(1),imageSize(2),num_images);
+videoImages = zeros(imageSize(1),imageSize(2),1,num_images);
+for k = 1:num_images
+    A = im2double(imread(fname, k, 'Info', info));
+    allImages(:,:,k) = A;
+    videoImages(:,:,1,k) = A;
+    % ... Do something with image A ...
+end
+meanImage = mean(allImages,3);
+imagesc(meanImage);
+
+writerObj = VideoWriter('rabbit_video_2.avi');
+writerObj.FrameRate = 3;
+open(writerObj);
+writeVideo(writerObj,videoImages);
+close(writerObj);
