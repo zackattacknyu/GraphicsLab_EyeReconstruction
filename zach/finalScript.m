905c1a64d66c36ef21c16396b2926c687a5e1a3d
zackattacknyu <zackattacknyu@gmail.com>
2014-05-29 18:32:12 -0700

    generated 3d data set well

--- zach/finalScript.m
+++ zach/finalScript.m
@@ -1,29 +1,56 @@
 
-image1 = im2double(imread('rabbitImages/image1.jpg'));
+firstImage = im2double(imread('rabbitImages/image1.jpg'));
+sizeImage = size(firstImage);
+height = sizeImage(1);
+width = sizeImage(2);
 
-%filter = fspecial('gaussian',5);
-filter = fspecial('average',3);
-newImage1 = conv2(image1,filter,'same');
-
-image1Seg = getSegImage(newImage1);
-figure
-imshow(image1Seg);
-
-numPixels = sum(sum(image1Seg));
-xVals = zeros(1,numPixels);
-yVals = zeros(1,numPixels);
-zVals = zeros(1,numPixels);
+numImages = 10;
 index = 1;
-for x = 1:width
-   for y = 1:height
-      if(image1Seg(y,x)==1)
-         xVals(index) = x;
-         yVals(index) = y;
-         zVals(index) = 1;
-         index = index + 1;
-      end
-   end
+numPossibleTotalValues = numImages*height*width*2;
+
+xVals = zeros(1,numPossibleTotalValues);
+yVals = zeros(1,numPossibleTotalValues);
+zVals = zeros(1,numPossibleTotalValues);
+
+prevImageSeg = zeros(sizeImage);
+
+for num = 1:numImages
+    
+    imname = strcat('rabbitImages/image',num2str(num),'.jpg');
+    image1 = im2double(imread(imname));
+
+    %filter = fspecial('gaussian',5);
+    filter = fspecial('average',3);
+    newImage1 = conv2(image1,filter,'same');
+
+    image1Seg = getSegImage(newImage1);
+    
+    image01Seg = prevImageSeg&image1Seg;
+        
+    for x = 1:width
+       for y = 1:height
+          if(image1Seg(y,x)==1)
+             xVals(index) = x;
+             yVals(index) = y;
+             zVals(index) = num;
+             index = index + 1;
+          end
+          if(image01Seg(y,x)==1)
+             xVals(index) = x;
+             yVals(index) = y;
+             zVals(index) = num-0.5;
+             index = index + 1;
+          end
+       end
+    end
+    
+    prevImageSeg = image1Seg;
+    
 end
 
-figure
+xVals = xVals(1:index);
+yVals = yVals(1:index);
+zVals = zVals(1:index);
+
 plot3(xVals,yVals,zVals,'r.');
+
