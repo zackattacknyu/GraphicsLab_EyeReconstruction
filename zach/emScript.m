69a1942853283c514b941036f827494bd9971ca0
zackattacknyu <zackattacknyu@gmail.com>
2014-05-31 16:41:44 -0700

    got em script working

---
+++ zach/emScript.m
@@ -0,0 +1,48 @@
+
+image1 = im2double(imread('rabbitImages/image1.jpg'));
+
+%filter = fspecial('gaussian',5);
+filter = fspecial('average',3);
+newImage1 = conv2(image1,filter,'same');
+
+image1Size = size(image1);
+height = image1Size(1);
+width = image1Size(2);
+
+xVals = 1:width;
+xVals = xVals/width;
+xValsArray = repmat(xVals,[height 1]);
+yVals = 1:height;
+yVals = yVals/height;
+yVals = transpose(yVals);
+yValsArray = repmat(yVals,[1 width]);
+
+image1Data = zeros(height,width,3);
+image1Data(:,:,1) = newImage1;
+image1Data(:,:,2) = xValsArray;
+image1Data(:,:,3) = yValsArray;
+
+N = height*width;
+K = 10;
+method = 1;
+image1DataSeq = reshape(image1Data,[N 3]);
+[label,model,llh] = emgm(transpose(image1DataSeq),20);
+
+threshold = 0.25;
+indices = label;
+cluster = model.mu;
+clusterValues = labelValues(1,:);
+clusterSeg = double(clusterValues>threshold);
+image1Seg = zeros(1,N);
+image1NonSeg = zeros(1,N);
+for index = 1:N
+    image1Seg(index) = clusterSeg(indices(index));
+    image1NonSeg(index) = clusterValues(indices(index));
+end
+image1Seg = reshape(image1Seg,[height width]);
+figure
+imshow(image1Seg);
+image1NonSeg = reshape(image1NonSeg,[height width]);
+figure
+imagesc(image1NonSeg);
+colorbar;
